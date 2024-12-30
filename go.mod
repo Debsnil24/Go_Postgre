4caac7c17364ca82769a5106b1200a8ed4789e2d
Debsnil24 <debsnilsamudra@gmail.com>
2024-12-31 04:07:40 +0530

    Updated the Router Package **Summary** - Added the code for Handling routes and their respective functions **To-Do** - Create the code for Web-Server in the main package - Create the code for the models package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Debsnil24/Go_Postgre.git
 
 go 1.23.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- router/router.go
+++ router/router.go
@@ -1,2 +1,16 @@
 package router
 
+import (
+	"github.com/Debsnil24/Go_Postgre.git/middleware"
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+	r := mux.NewRouter()
+	
+	r.HandleFunc("/api/stock/{id}", middleware.GetStock).Methods("GET", "OPTIONS")
+	r.HandleFunc("/api/stock", middleware.GetAllStock).Methods("GET","OPTIONS")
+	r.HandleFunc("/api/newstock", middleware.CreateStock).Methods("POST", "OPTIONS")
+	r.HandleFunc("/api/stock/{id}", middleware.UpdateStock).Methods("PUT", "OPTIONS")
+	r.HandleFunc("/api/stock/{id}", middleware.DeleteStock).Methods("DELETE", "OPTIONS")
+}
