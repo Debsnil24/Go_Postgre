2982b5522932a5ac2123d4f1a56fc1f42a2c76b9
Debsnil24 <debsnilsamudra@gmail.com>
2025-01-02 08:46:19 +0530

    Updated the Main & Models Package **Summary** - Added the code for initializing the localhost webserver on port 8080 in the Main Package - Defined a type struct for the stocks in the Models Package **To-Do** - Create the code for handling the data in the Middleware Package

--- main/main.go
+++ main/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Debsnil24/Go_Postgre.git/router"
+)
+
+func main() {
+	r := router.Router()
+	fmt.Println("Starting Server on Port 8080.....")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
--- models/models.go
+++ models/models.go
@@ -1,1 +1,8 @@
 package models
+
+type Stock struct{
+	StockID int `json:"stockid"`
+	Name string `json:"name"`
+	Price int `json:"price"`
+	Company string `json:"company"`
+}
